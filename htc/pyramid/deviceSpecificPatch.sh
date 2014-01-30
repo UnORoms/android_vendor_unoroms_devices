8386c2b77e348355eaad321affe1cc80e1af4311
Shantur Rathore <rathore4u@gmail.com>
2014-01-30 22:59:59 +0000

    Omni build scripts are not happy with AndroidProducts.mk so it will be copied as part of patch

--- htc/pyramid/deviceSpecificPatch.sh
+++ htc/pyramid/deviceSpecificPatch.sh
@@ -1,3 +1,4 @@
 cd device/htc/pyramid 
 patch -p1 < ../../../vendor/unoroms/devices/htc/pyramid/goo-details.patch
 cd -
+cp vendor/unoroms/devices/htc/pyramid/overrides/AndroidProducts.bak device/htc/pyramid/AndroidProducts.mk
