58a3639472994205824c56b5026bb2c58bdb9899
Shantur Rathore <rathore4u@gmail.com>
2014-01-30 23:12:40 +0000

    Added cpu scorpion is not supported by Omni so patching to cortex-a9

--- htc/pyramid/deviceSpecificPatch.sh
+++ htc/pyramid/deviceSpecificPatch.sh
@@ -1,4 +1,5 @@
 cd device/htc/pyramid 
 patch -p1 < ../../../vendor/unoroms/devices/htc/pyramid/goo-details.patch
+patch -p1 < ../../../vendor/unoroms/devices/htc/pyramid/cpu-variant.patch
 cd -
 cp vendor/unoroms/devices/htc/pyramid/overrides/AndroidProducts.bak device/htc/pyramid/AndroidProducts.mk
