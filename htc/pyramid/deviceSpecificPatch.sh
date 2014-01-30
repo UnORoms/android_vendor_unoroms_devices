b8d5241867507a47d8b2cd7ee9a7248e3a7cd478
Shantur Rathore <rathore4u@gmail.com>
2014-01-30 21:42:04 +0000

    Convert to SlimRom

--- htc/pyramid/deviceSpecificPatch.sh
+++ htc/pyramid/deviceSpecificPatch.sh
@@ -1,3 +1,4 @@
 cd device/htc/pyramid 
 patch -p1 < ../../../vendor/unoroms/devices/htc/pyramid/goo-details.patch
+patch -p1 < ../../../vendor/unoroms/devices/htc/pyramid/overlay-menu-key.patch
 cd -
