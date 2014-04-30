212f3d3510608f69b6408a6c39f4767727ebc9f5
Shantur Rathore <rathore4u@gmail.com>
2014-04-30 10:34:03 +0100

    Move to Flemmard base

--- htc/pyramid/deviceSpecificPatch.sh
+++ htc/pyramid/deviceSpecificPatch.sh
@@ -1,12 +1,3 @@
-cd device/htc/pyramid 
-patch -p1 < ../../../vendor/unoroms/devices/htc/pyramid/goo-details.patch
-patch -p1 < ../../../vendor/unoroms/devices/htc/pyramid/cpu-variant.patch
-cd -
-
-cd bionic
-git checkout 69ea65ce8aefd77e3a42202352a468775c843bca
-cd -
-
 cd system/vold
 patch -p1 < ../../vendor/unoroms/devices/htc/pyramid/vold.patch
 cd -
