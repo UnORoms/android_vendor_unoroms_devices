85fecff1577e58666832720cd9d13fa5abcc7016
Shantur Rathore <rathore4u@gmail.com>
2014-04-30 15:19:25 +0100

    Fix overlays

--- htc/pyramid/deviceSpecificPatch.sh
+++ htc/pyramid/deviceSpecificPatch.sh
@@ -6,4 +6,8 @@ cd device/htc/msm8660-common
 patch -p1 < ../../../vendor/unoroms/devices/htc/pyramid/sepolicy.patch
 cd -
 
+cd device/htc/pyramid
+patch -p1 < ../../../vendor/unoroms/devices/htc/pyramid/overlay.patch
+cd -
+
 cp vendor/unoroms/devices/htc/pyramid/overrides/AndroidProducts.bak device/htc/pyramid/AndroidProducts.mk
