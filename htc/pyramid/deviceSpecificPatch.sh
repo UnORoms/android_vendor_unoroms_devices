3f560301847a104786e731cd1ae333f62d89ba58
Shantur Rathore <rathore4u@gmail.com>
2014-04-30 14:35:49 +0100

    Fix sepolicy for OmniRom

--- htc/pyramid/deviceSpecificPatch.sh
+++ htc/pyramid/deviceSpecificPatch.sh
@@ -2,4 +2,8 @@ cd system/vold
 patch -p1 < ../../vendor/unoroms/devices/htc/pyramid/vold.patch
 cd -
 
+cd device/htc/msm8660-common
+patch -p1 < ../../../vendor/unoroms/devices/htc/pyramid/sepolicy.patch
+cd -
+
 cp vendor/unoroms/devices/htc/pyramid/overrides/AndroidProducts.bak device/htc/pyramid/AndroidProducts.mk
