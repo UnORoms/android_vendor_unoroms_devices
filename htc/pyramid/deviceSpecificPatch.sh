756a9187e556eddd505c1680ab9903afa6a523d0
Shantur Rathore <rathore4u@gmail.com>
2014-04-30 13:01:45 +0100

    Fixed derp

--- htc/pyramid/deviceSpecificPatch.sh
+++ htc/pyramid/deviceSpecificPatch.sh
@@ -1,4 +1,7 @@
 cd device/htc/pyramid 
 patch -p1 < ../../../vendor/unoroms/devices/htc/pyramid/overlay-menu-key.patch
+cd -
+
+cd device/htc/msm8660-common 
 patch -p1 < ../../../vendor/unoroms/devices/htc/pyramid/sepolicy.patch
 cd -
